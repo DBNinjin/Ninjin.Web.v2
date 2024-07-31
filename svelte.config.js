3be0b5f5647fa601e1a2eb76921005025c0c41ce
CodeKyana <169302941+anotherfrench@users.noreply.github.com>
2024-07-31 18:23:17 +0200

    Prepare to GH Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: ''
+		}
 	},
 	preprocess: vitePreprocess()
 };
