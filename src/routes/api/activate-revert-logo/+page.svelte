3c3585f37455712f4614bf433be2189655332801
Luneuh <169302941+Luneuh@users.noreply.github.com>
2024-12-08 13:13:04 +0100

    Update +page.svelte

--- src/routes/api/activate-revert-logo/+page.svelte
+++ src/routes/api/activate-revert-logo/+page.svelte
@@ -9,9 +9,9 @@
 >
 	<div class="mx-auto max-w-md text-center">
 		<div class="mx-auto h-12 w-12 text-primary" />
-		<h1 class="mt-4 text-6xl font-bold tracking-tight text-foreground sm:text-7xl">NQ@EG</h1>
+		<h1 class="mt-4 text-6xl font-bold tracking-tight text-foreground sm:text-7xl">NINJIN@EG1</h1>
 		<p class="mt-4 text-muted-foreground">
-			Oups, je crois que tu as trouver l'easter egg d'A.F <br /> dénommée "Revert Logo".
+			Oups, je crois que tu as trouver l'easter egg de Luneuh <br /> dénommée "Revert Logo".
 		</p>
 		<div class="mt-6">
 			<a
